a6ace3f48af5d9254e5be760fc96b869651a9c54
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-11-21 20:49:06 +0000

    Working crossing system

--- Makefile
+++ Makefile
@@ -2,9 +2,17 @@ Image=BUILD/K64F/GCC_ARM/kf6010-mini-project.bin
 
 all: crossing.pdf $(Image)
 
-crossing.pdf:crossing.tex
+crossing.pdf: crossing.tex
 	latexmk -pdf -quiet crossing
 
 
-$(Image):src/main.cpp
+$(Image): mbed-os src/main.cpp
 	mbed compile
+
+mbed-os: mbed-os.lib
+	mbed deploy
+
+.PHONY: flash
+flash: 
+	mbed compile --flash
+
--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,108 @@
 
 #include <mbed.h>
 
+enum { On, Off }; /* sneaky hack to handle common anode LEDs */
+
+/* Locks and signals */
+Mutex crossing; /* who holds the mutex can use the crossing */
+Semaphore tocross; /* indicate that pedestians want to cross */
+Semaphore bleeping;
+
+/* Pedestrian task */
+void pedestrians(void)
+{
+	DigitalOut go(LED_GREEN,Off);
+	DigitalOut stop(LED_RED,On);
+
+	while(true) {
+		crossing.lock(); /*  cannot proceed until released by the traffic */
+		wait(1);
+		/* Red -> green */
+		stop = Off;
+		go = On;
+		bleeping.release();
+		/* allow pedestrains to cross */
+		wait(5);
+		bleeping.wait();
+		/* green -> red */
+		stop = On;
+		go = Off;
+		wait(2);
+		crossing.unlock();
+	}
+}
+
+/* Traffic task */
+void traffic(void)
+{
+	DigitalOut red(D5, On);
+	DigitalOut amber(D8,Off);
+	DigitalOut green(D9,Off);
+	while(true) {
+		red = On;
+		amber = green = Off;
+
+		crossing.lock();
+		wait(1.5);
+		/* Red -> green lights */
+		red = amber = On;
+		green = Off;
+		wait(2.0); /* wait for 3s */
+		red = amber = Off;
+		green = On;
+
+		tocross.wait(); /* wait for pedestrians wanting to cross */
+		
+		/* Green -> red lights */
+		wait(1);
+		red = green = Off;
+		amber = On;
+		wait(3.0);
+
+		red = On;  /* make sure red is on before releasing lock */
+		amber = green = Off;
+		wait(1.5);	
+		crossing.unlock();
+	}
+}
+
+/* Interrupt handler for button */
+void pressed(void)
+{
+	/* release the semaphore signalling pedestians waiting */
+	tocross.release();
+}
+
+void bleeper(void)
+{
+	DigitalOut beep(LED_BLUE,Off);
+	PwmOut b(D6);
+	b.period_us(500);
+	b = 0;
+	while(true) {
+		bleeping.wait();
+		//beep = On;
+		b = 0.5;
+		bleeping.release();
+		wait(0.6);
+		//beep = Off;
+		b = 0;
+		wait(0.4);
+	}
+}
 
 int main(void)
 {
+	InterruptIn button_a(SW2);
+	InterruptIn button_b(SW3);
+	Thread cars, people, beeps;
+
+	button_a.fall( pressed );
+	button_b.fall( pressed );
+	cars.start( traffic );
+	people.start( pedestrians );
+	beeps.start(bleeper);
+
+	cars.join();
 }
 
