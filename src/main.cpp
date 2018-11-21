694710be12672d78fd56095ea00d918da997180a
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-11-21 14:36:16 +0000

    Notes and timing diagram

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/* KF6010 -- Distributed real-time systems
+ * End of Semester One mini project
+ * weeks 11&12 (2hours in labs)
+ */
+
+/* Dr Alun Moon
+ * November 2018
+ */
+
+#include <mbed.h>
+
+
+int main(void)
+{
+}
+
